568b9277e515bfd0753cf01867e4db3bd773635f
Lars Brinkhoff <lars@nocrew.org>
2016-01-04 21:12:16 +0100

    Fix copyright test for renamed files.

--- test/test-copyright.sh
+++ test/test-copyright.sh
@@ -3,9 +3,11 @@ fail() {
     exit 1
 }
 
+logopts="-1 --follow --diff-filter=M --format=format:%aD"
+
 find . -name '*.fth' | while read i; do
    if grep 'Copyright .* Brinkhoff' "$i" >/dev/null; then
-     d=`git log -1 --format=format:%aD $i | cut -d' ' -f4`
+     d=`git log $logopts $i | cut -d' ' -f4`
      grep "$d" $i >/dev/null || fail "$i" "$d"
    fi
 done
